936d1651d6a49f8d9dd99ac779eb5048b0634f79
Ivy K <98768550+ivykos@users.noreply.github.com>
2022-02-25 15:31:23 -0500

    Add files via upload

--- R/reading.R
+++ R/reading.R
@@ -4,27 +4,24 @@
 #' @export
 
 read_h5 <- function(h5, proj, a){
-  h5_file <- Read10X_h5(h5, use.names = FALSE) 
+  h5_file <- Read10X_h5(h5, use.names = TRUE) 
   h5.seurat <- CreateSeuratObject(counts = h5_file, project = proj, assay = a)
   h5.seurat <- NormalizeData(h5.seurat)
   h5.seurat <- FindVariableFeatures(h5.seurat, selection.method = "vst", nfeatures = 2000)
-  all.genes <- rownames(h5.seurat)
-  h5.seurat <- ScaleData(h5.seurat, features = all.genes)
+  h5.seurat <- ScaleData(h5.seurat)
   h5.seurat <- RunPCA(h5.seurat, features = VariableFeatures(object = h5.seurat))
  
 }
 
+
 #' @export
-get_umap <- function(seuratObj, dimensions, res){
+transfer_clusters <- function(seuratObj, proj, dimensions, res, tissue_csv){
   seuratObj <- FindNeighbors(seuratObj, dims = 1:dimensions)
   seuratObj <- FindClusters(seuratObj, resolution = res)
   
   seuratObj <- RunUMAP(seuratObj, dims = 1:dimensions)
-  DimPlot(seuratObj, reduction = "umap")
-}
-
-#' @export
-transfer_clusters <- function(seuratObj, proj, tissue_csv){
+  
+  
   write.table(seuratObj@active.ident, file="tmp.tsv", quote=FALSE, sep="\t", col.names = FALSE)
   idents <- read.delim("tmp.tsv", header = FALSE)
   file.remove("tmp.tsv")
@@ -40,6 +37,7 @@ transfer_clusters <- function(seuratObj, proj, tissue_csv){
   cluster.ordered <- table[order(table$V1),]
   pos.ordered <-positions[order(positions$V1),]
   
-  ggplot(pos.ordered, aes(x=pos.ordered$V3, y=pos.ordered$V4, color=as.factor(table$V1))) + 
+  
+  ggplot(pos.ordered, aes(x=pos.ordered$V3, y=pos.ordered$V4, color=as.factor(table$V2))) + 
     geom_point() + theme_linedraw() +ggtitle(as.character(proj))
 }
