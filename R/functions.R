6eaf6bf22df22f1e2802ca830749e3a7a69d16d1
Ivy K <98768550+ivykos@users.noreply.github.com>
2022-03-05 11:28:44 -0500

    Add files via upload

--- R/functions.R
+++ R/functions.R
@@ -47,11 +47,30 @@ transfer_clusters <- function(seuratObj, proj, dimensions, res, tissue_csv){
 }
 
 #' @export
-get_expression <- function(seurat_obj, feature){
-  tmp <- as.matrix(GetAssayData(object = seurat_obj, slot = "counts"))
+get_expression <- function(seuratObj, feature){
+  tmp <- as.matrix(GetAssayData(object = seuratObj, slot = "counts"))
   feature_count <- tmp[feature,]
   rotated <- as.data.frame(t(feature_count))
   table.c["Expr"] <- rotated$feature_count
+ 
+  #Get tissue positions again 
+  write.table(seuratObj@active.ident, file="tmp.tsv", quote=FALSE, sep="\t", col.names = FALSE)
+  idents <- read.delim("tmp.tsv", header = FALSE)
+  file.remove("tmp.tsv")
+  write.table(seuratObj@meta.data$orig.ident, file="tmp.tsv", quote=FALSE, sep="\t", col.names = FALSE)
+  orig <- read.delim("tmp.tsv", header = FALSE)
+  file.remove("tmp.tsv")
   
+  positions <- read.csv(tissue_csv, header = F)
+  positions <- positions[positions$V2 == 1,]
+  
+  idents["Origin"] = orig$V2
+  table <- idents[idents$Origin == proj,]
+  cluster.ordered <- table[order(table$V1),]
+  pos.ordered <-positions[order(positions$V1),]
+  
+  ggplot(pos.ordered, aes(pos.ordered$V3, pos.ordered$V4)) + 
+    geom_point(aes(color = table$Expr), size = 2) + 
+    scale_color_viridis(option = "inferno") + theme_bw()+ ggtitle("APOE4 Gad1")
 }
 
