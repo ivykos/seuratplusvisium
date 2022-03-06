f72e41eaa9f3f2f961bc1f9629ab45e0485245d7
Ivy K <98768550+ivykos@users.noreply.github.com>
2022-03-06 12:27:07 -0500

    Add files via upload

--- R/functions.R
+++ R/functions.R
@@ -41,7 +41,7 @@ transfer_clusters <- function(seuratObj, proj, dimensions, res, tissue_csv){
   
   seuratObj <- RunUMAP(seuratObj, dims = 1:dimensions)
   
-  
+  #Retrieve the cluster labels and project ID from the Seurat object and put them in tables
   write.table(seuratObj@active.ident, file="tmp.tsv", quote=FALSE, sep="\t", col.names = FALSE)
   idents <- read.delim("tmp.tsv", header = FALSE)
   file.remove("tmp.tsv")
@@ -49,15 +49,22 @@ transfer_clusters <- function(seuratObj, proj, dimensions, res, tissue_csv){
   orig <- read.delim("tmp.tsv", header = FALSE)
   file.remove("tmp.tsv")
   
+  #Get the coordinates for plotting the tissue image
   positions <- read.csv(tissue_csv, header = F)
+  
+  #Make sure only the spots within the tissue are included for this analysis
   positions <- positions[positions$V2 == 1,]
   
+  #Put everything in one table for simplicity
   idents["Origin"] = orig$V2
+  
+  #"table" is the table we will use from now on
   table <- idents[idents$Origin == proj,]
+  
   cluster.ordered <- table[order(table$V1),]
   pos.ordered <-positions[order(positions$V1),]
   
-  
+  #Plot it
   ggplot(pos.ordered, aes(x=pos.ordered$V3, y=pos.ordered$V4, color=as.factor(table$V2))) + 
     geom_point() + theme_linedraw() +ggtitle(as.character(proj))
 }
@@ -72,6 +79,7 @@ transfer_clusters <- function(seuratObj, proj, dimensions, res, tissue_csv){
 #' get_expression(Obj,"my_project","Ripk1","tissue_positions.csv")
 #' @export
 get_expression <- function(seuratObj, proj, feature, tissue_csv){
+  
   #Get tissue positions again 
   write.table(seuratObj@active.ident, file="tmp.tsv", quote=FALSE, sep="\t", col.names = FALSE)
   idents <- read.delim("tmp.tsv", header = FALSE)
@@ -88,10 +96,12 @@ get_expression <- function(seuratObj, proj, feature, tissue_csv){
   cluster.ordered <- table[order(table$V1),]
   pos.ordered <-positions[order(positions$V1),]
   
+  #Extract the counts matrix from the Seurat object
   tmp <- as.matrix(GetAssayData(object = seuratObj, slot = "counts"))
   feature_count <- tmp[feature,]
   rotated <- as.data.frame(t(feature_count))
-  table["Expr"] <- rotated$feature_count
+  rotated <- as.data.frame(t(feature_count))
+  table["Expr"] <- rotated$V1
   
   ggplot(pos.ordered, aes(pos.ordered$V3, pos.ordered$V4)) + 
     geom_point(aes(color = table$Expr), size = 2) + 
